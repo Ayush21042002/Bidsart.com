58c45c8fa3b6b38223f0701f2bed8ec0cd0c4bb8
Ayush21042002 <51852239+Ayush21042002@users.noreply.github.com>
2020-09-15 21:24:21 +0530

    added product fetch queries

--- Backend/controllers/fetch-products.controller.js
+++ Backend/controllers/fetch-products.controller.js
@@ -1,22 +1,23 @@
 const con = require('../database/db');
 
-exports.getAllProducts = (req,res) => {
-    const query = "SELECT * FROM product";
-    con.query(query, (err,result) => {
-        if(err){
-            throw err;
-        }
-        console.log(result);
-        res.send("successful");
+exports.getAllProducts = (req,res,next) => {
+    
+    con.query("SELECT * FROM products order by productId; SELECT * from image order by productId;", (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(result);
+      res.json({products: result[0], images: result[1]});
     });
 };
 
 exports.getProductByid = (req,res) => {
-    const query = "SELECT * FROM product where product_id = ?";
-    con.query(query, [req.params.id],(err,result) => {
+    const query = "SELECT * FROM products where productId = ?; SELECT * FROM image where productId = ?";
+    console.log(req.params.id);
+    con.query(query, [req.params.id,req.params.id],(err,result) => {
         if(err) throw err;
 
         console.log(result);
-        res.send("Successful");
+        res.json({product: result[0], images: result[1]});
     }) 
 };
