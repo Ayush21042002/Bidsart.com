bcd2ad87f1be4f8cf0559ef74fbc83a78d937d82
Ayush21042002 <51852239+Ayush21042002@users.noreply.github.com>
2020-09-16 18:41:38 +0530

    redefined the response of /product

--- Backend/controllers/fetch-products.controller.js
+++ Backend/controllers/fetch-products.controller.js
@@ -4,37 +4,43 @@ exports.getAllProducts = (req,res,next) => {
     
     con.query("SELECT * FROM products order by productId; SELECT * from image order by productId;", (err, result) => {
       if (err) {
-        console.log(err);
+        throw err;
       }
     //   console.log(result);
       const resp = [];
       for(let i=0;i<result[0].length;i++){
         const product = {
-            product: result[0][i],
-            images: result[1].filter(item => item.productId == result[0][i].productId)
-        }
+          productId: result[0][i].productId,
+          title: result[0][i].title,
+          description: result[0][i].description,
+          category: result[0][i].category,
+          images: result[1].filter( (item) => item.productId == result[0][i].productId),
+        };
         resp.push(product);
       }
-      console.log(resp,"This is the result");
+      // console.log(resp);
       res.json(resp);
     });
 };
 
 exports.getProductByid = (req,res) => {
     const query = "SELECT * FROM products where productId = ?; SELECT * FROM image where productId = ?";
-    console.log(req.params.id);
+    // console.log(req.params.id);
     con.query(query, [req.params.id,req.params.id],(err,result) => {
         if(err) throw err;
 
         const resp = [];
         for(let i=0;i<result[0].length;i++){
             const product = {
-                product: result[0][i],
+                productId: result[0][i].productId,
+                title: result[0][i].title,
+                description: result[0][i].description,
+                category: result[0][i].category,
                 images: result[1].filter(item => item.productId == result[0][i].productId)
             }
             resp.push(product);
         }
-      console.log(resp,"This is the result");
+      // console.log(resp);
       res.json(resp);
     });
 }; 
