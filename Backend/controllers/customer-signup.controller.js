50f005df12911c3b67ebf185ee3e2cbbf605f68f
Ayush21042002 <51852239+Ayush21042002@users.noreply.github.com>
2020-09-17 19:10:57 +0530

    modified customer-signup

--- Backend/controllers/customer-signup.controller.js
+++ Backend/controllers/customer-signup.controller.js
@@ -5,13 +5,11 @@ const con = require('../database/db');
 exports.createCustomer = (req, resp) => {
     //   console.log(req.body);
   _Bcrypt.hash(req.body.password, 10).then((hash) => {
+    console.log(req.body);
     const user = {
         email: req.body.email,
         password:  hash
     }
-    const fname = req.body.fname;
-    const mname = req.body.mname;
-    const lname = req.body.lname;
     con.query(
       "INSERT INTO user SET ?",user,
       (err, result) => {
@@ -19,15 +17,28 @@ exports.createCustomer = (req, resp) => {
         const userId = result.insertId;
         const customer = {
             customerId: userId,
-            fname: fname,
-            mname: mname,
-            lname: lname,
+            fname: req.body.fname,
+            mname: req.body.mname,
+            lname: req.body.lname,
         };
-        con.query("INSERT INTO customer SET ?",customer,(err,res) => {
-            if(err) throw err;
-            resp.send("SUCCESSFULL");
-            console.log(res);
+
+        const contact = {
+          customerId: userId,
+          contact: req.body.contact,
+          countrycode: req.body.countrycode,
+        };
+
+        con.query("INSERT INTO customer SET ?", customer, (err, res) => {
+          if (err) throw err;
+          console.log(res);
         });
+
+        con.query("INSERT INTO contact SET ?", contact, (err, res) => {
+          if (err) throw err;
+          console.log(res);
+        });
+
+        resp.send("HELLO");
         console.log(result);
       }
     );
