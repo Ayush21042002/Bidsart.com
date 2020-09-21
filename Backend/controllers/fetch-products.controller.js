efab7c09158e275e6c6a3352695508512c5e6b5b
Ayush21042002 <51852239+Ayush21042002@users.noreply.github.com>
2020-09-21 16:57:04 +0530

    added seller products functionality

--- Backend/controllers/fetch-products.controller.js
+++ Backend/controllers/fetch-products.controller.js
@@ -23,7 +23,7 @@ exports.getAllProducts = (req,res,next) => {
     });
 };
 
-exports.getProductByid = (req,res) => {
+exports.getProductByProductId = (req,res) => {
     const query = "SELECT * FROM products where productId = ?; SELECT * FROM image where productId = ?";
     // console.log(req.params.id);
     con.query(query, [req.params.id,req.params.id],(err,result) => {
@@ -44,3 +44,28 @@ exports.getProductByid = (req,res) => {
       res.json(resp);
     });
 }; 
+
+exports.getAllProductsBySeller = (req,res) => {
+    const sellerId = req.params.sellerId;
+
+  con.query("SELECT * FROM products where productId IN (SELECT productId from has_products where sellerId = ?); SELECT * FROM image where productId IN (SELECT productId from has_products where sellerId = ?);",
+    [sellerId,sellerId], (err,result)=> {
+
+      if(err) throw err;
+
+      const resp = [];
+      for (let i = 0; i < result[0].length; i++) {
+        const product = {
+          productId: result[0][i].productId,
+          title: result[0][i].title,
+          description: result[0][i].description,
+          category: result[0][i].category,
+          images: result[1].filter(item => item.productId == result[0][i].productId)
+        }
+        resp.push(product);
+      }
+      // console.log(resp);
+      res.json(resp);
+
+    });
+};
