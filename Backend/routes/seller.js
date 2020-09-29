c2a148ce87e236a77a8d4650b9865ce4800072c9
Ayush21042002 <51852239+Ayush21042002@users.noreply.github.com>
2020-09-29 09:27:43 +0530

    seller-login added

--- Backend/routes/seller.js
+++ Backend/routes/seller.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const sellerAuth = require('../Middleware/verify-seller');
 const sellerSignup = require("../controllers/seller-signup.controller");
+const sellerLogin = require("../controllers/seller-login.controller");
 const Router = express.Router();
 
 Router.get('/:id',sellerAuth, (req,res) => {
     // Here the seller details will be sent to seller
 });
 
-Router.post('/login', (req,res) => {
-    // Here the login request of seller will be handled
-});
+Router.post('/login',sellerLogin.sellerLogin);
 
 Router.post('/signup', sellerSignup.createSeller);
 
