d5070b55e2a4d1e80899b2eee0eb2be4a8de56a6
yashkumar3000 <yashkumar20009@gmail.com>
2020-10-03 19:28:44 +0530

    fetching of seller details

---
+++ Backend/controllers/fetch-seller.customer.js
@@ -0,0 +1,29 @@
+const con = require("../database/db") ;
+
+exports.getAllSellers = (err,res)=>{
+  con.query("SELECT * from seller",(err,result)=>{
+      if(err) throw err;
+      if(result.length>0){
+          res.status(200).json(result);
+      }else{  
+       res.status(200).send("NO Sellers");
+      }
+  } );
+};
+
+exports.getSellerById = (req,res) => {
+  const sid = req.params.id;
+  
+  con.query(
+    "select sid, building_no,add1,add2, state , country,zip ,landmark ,pan_num ,aadhar_num from seller where sid = ?",
+    [sid], (err,result) => {
+        if(err) throw err;
+
+        if(result.length > 0){
+            res.status(200).json(result[0]);
+        }else{
+            res.status(300).send("No Such Seller");
+        }
+    }
+  );
+};
