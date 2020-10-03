66efd909600a1c2cf1dc44f671af7c1c8e92c17b
yashkumar3000 <yashkumar20009@gmail.com>
2020-10-03 21:31:11 +0530

    update product error handled

--- Backend/controllers/update-product.controller.js
+++ Backend/controllers/update-product.controller.js
@@ -4,8 +4,9 @@ exports.updateProduct= (req,res) => {
     const title = req.body.title;
     const description = req.body.description; 
     const category = req.body.category;
+    const productId = req.body.productId;
  
-    con.query( "UPDATE products SET title= ? ,description = ? ,category = ?  where productId= ? " ,[title,description,category],  
+    con.query( "UPDATE products SET title= ? ,description = ? ,category = ? where productId= ? " ,[title,description,category,productId],  
         (err,result) => {
            if(err) throw err ; 
            res.json({msg:"Updated successfully......"}); 
