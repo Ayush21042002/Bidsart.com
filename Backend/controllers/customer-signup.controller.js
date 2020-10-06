e28bece67a8f7d697264bc23eec4c8e174558aaa
Ayush21042002 <51852239+Ayush21042002@users.noreply.github.com>
2020-10-06 17:26:39 +0530

    added feature of having same email for both accounts

--- Backend/controllers/customer-signup.controller.js
+++ Backend/controllers/customer-signup.controller.js
@@ -10,47 +10,130 @@ exports.createCustomer = (req, resp) => {
         email: req.body.email,
         password:  hash
     }
-    con.query(
-      "INSERT INTO user SET ?",user,
-      (err, result) => {
-        if (err) throw err;
-        const userId = result.insertId;
-        const customer = {
-            customerId: userId,
-            fname: req.body.fname,
-            mname: req.body.mname,
-            lname: req.body.lname,
-        };
 
-        const contact = {
-          customerId: userId,
-          contact: req.body.contact,
-          countrycode: req.body.countrycode,
-        };
-        
-        const cust_address = {
-          customerId: userId,
-          houseNo: req.body.houseno,
-          addressLine: req.body.addressLine,
-          city: req.body.city,
-          state: req.body.state,
-          country: req.body.country,
-          landmark: req.body.landmark,
-        }
+    con.query("SELECT * FROM user where email = ?",[user.email], (err,searchResult) => {
+      if(err) throw err;
 
-        con.query("INSERT INTO customer SET ?", customer, (err, res) => {
-          if (err) throw err;
-          // console.log(res);
-          con.query("INSERT INTO contact SET ?", contact, (err, res) => {
-            if (err) throw err;
-            // console.log(res);
-            con.query("INSERT INTO cust_address SET ?", cust_address, (err, res) => {
-              if (err) throw err;
-              // console.log(res);
-              resp.send("Successfully added");
-            });
-          });
+      if(searchResult.length > 0){
+        const userId = searchResult[0].userId;
+
+        con.query("SELECT * FROM customer where customerId = ?",[userId], (err,customerResult) => {
+          if(err) throw err;
+
+          if(customerResult.length > 0){
+            resp.json({
+              message: "User already registered"
+            })
+          } else{
+            const customer = {
+              customerId: userId,
+              fname: req.body.fname,
+              mname: req.body.mname,
+              lname: req.body.lname,
+            };
+
+            const contact = {
+              customerId: userId,
+              contact: req.body.contact,
+              countrycode: req.body.countrycode,
+            };
+
+            const cust_address = {
+              customerId: userId,
+              houseNo: req.body.houseno,
+              addressLine: req.body.addressLine,
+              city: req.body.city,
+              state: req.body.state,
+              country: req.body.country,
+              landmark: req.body.landmark,
+            };
+
+            con.query(
+              "INSERT INTO customer SET ? ",
+              customer,
+              (err, customerResult) => {
+                if (err) throw err;
+
+                con.query(
+                  "INSERT INTO contact SET ? ",
+                  contact,
+                  (err, contactResult) => {
+                    if (err) throw err;
+
+                    con.query(
+                      "INSERT INTO cust_address SET ?",
+                      cust_address,
+                      (err, result) => {
+                        if (err) throw err;
+                        // console.log(res);
+                        resp.send("Successfully added");
+                      }
+                    );
+                  }
+                );
+              }
+            );
+          }
         });
-      });
-  });
+
+      } else{
+        con.query("INSERT INTO user SET ? ",user, (err,insertResult) => {
+          if(err) throw err;
+
+                  const userId = insertResult.userId;
+
+                  const customer = {
+                    customerId: userId,
+                    fname: req.body.fname,
+                    mname: req.body.mname,
+                    lname: req.body.lname,
+                  };
+
+                  const contact = {
+                    customerId: userId,
+                    contact: req.body.contact,
+                    countrycode: req.body.countrycode,
+                  };
+
+                  const cust_address = {
+                    customerId: userId,
+                    houseNo: req.body.houseno,
+                    addressLine: req.body.addressLine,
+                    city: req.body.city,
+                    state: req.body.state,
+                    country: req.body.country,
+                    landmark: req.body.landmark,
+                  };
+
+                  con.query(
+                    "INSERT INTO customer SET ? ",
+                    customer,
+                    (err, customerResult) => {
+                      if (err) throw err;
+
+                      con.query(
+                        "INSERT INTO contact SET ? ",
+                        contact,
+                        (err, contactResult) => {
+                          if (err) throw err;
+
+                          con.query(
+                            "INSERT INTO cust_address SET ?",
+                            cust_address,
+                            (err, result) => {
+                              if (err) throw err;
+                              // console.log(res);
+                              resp.send("Successfully added");
+                            }
+                          );
+                        }
+                      );
+                    }
+                  );
+
+        });
+      }
+    })
+
+});
 };
--- Backend/controllers/seller-signup.controller.js
+++ Backend/controllers/seller-signup.controller.js
@@ -12,33 +12,74 @@ exports.createSeller = (req, resp) => {
         password:  hash
     }
     // console.log(user);
-    con.query(
-        "INSERT INTO user SET ?",user,
-        (err,result)=> {    //result status after running sql query.e.g(1 row effected and all)
-            if(err) throw err;
-            const userId= result.insertId; //use of auto_increment in sid
-            const seller= {
-               sid:userId,
-               company_name:req.body.company_name,
-               building_no:Number(req.body.building_no),
-               add1:req.body.add1,
-               add2:req.body.add2,
-               state:req.body.state,
-               country:req.body.country,
-               zip:Number(req.body.zip),
-               landmark:req.body.landmark,
-               pan_num:req.body.pan_num,
-               aadhar_num:req.body.aadhar_num
-            };
-            // console.log(seller);
-            con.query("INSERT INTO seller SET ?",
-            seller,(err,res)=>{
-                if(err) throw err;
 
-                resp.send("Successfully added");
+        con.query("SELECT * FROM user WHERE email = ?",[user.email], (err,userResult) => {
+            if(err) throw err;
+
+            if(userResult.length > 0){
+                const userId = userResult[0].userId;
+
+                con.query("SELECT * FROM seller where sid = ?",[userId], (err,sellerSearch) => {
+                    if(err) throw err;
+
+                    if(sellerSearch.length > 0){
+                        resp.json({
+                            message: "Seller already registered"
+                        });
+                    }else{
+                        const seller = {
+                          sid: userId,
+                          company_name: req.body.company_name,
+                          building_no: Number(req.body.building_no),
+                          add1: req.body.add1,
+                          add2: req.body.add2,
+                          state: req.body.state,
+                          country: req.body.country,
+                          zip: Number(req.body.zip),
+                          landmark: req.body.landmark,
+                          pan_num: req.body.pan_num,
+                          aadhar_num: req.body.aadhar_num,
+                          contact1: req.body.contact1,
+                          contact2: req.body.contact2
+                        };
+                        // console.log(seller);
+                        con.query("INSERT INTO seller SET ?",seller,(err, res) => {
+                            if (err) throw err;
+
+                            resp.send("Successfully added");
+                        });
+                    }
+                });
+            }else{
+                con.query("INSERT INTO user SET ? ",user,(err,insertResult) => {
+                    if(err) throw err;
+
+                    const userId= result.insertId; //use of auto_increment in sid
+                    const seller = {
+                      sid: userId,
+                      company_name: req.body.company_name,
+                      building_no: Number(req.body.building_no),
+                      add1: req.body.add1,
+                      add2: req.body.add2,
+                      state: req.body.state,
+                      country: req.body.country,
+                      zip: Number(req.body.zip),
+                      landmark: req.body.landmark,
+                      pan_num: req.body.pan_num,
+                      aadhar_num: req.body.aadhar_num,
+                      contact1: req.body.contact1,
+                      contact2: req.body.contact2,
+                    };
+                    // console.log(seller);
+                    con.query("INSERT INTO seller SET ?",
+                    seller,(err,res)=>{
+                        if(err) throw err;
+
+                        resp.send("Successfully added");
+                    }
+                    )
+                });
             }
-             )
-        }
-    )
-  }
-  )};
+        });
+    }
+)};
