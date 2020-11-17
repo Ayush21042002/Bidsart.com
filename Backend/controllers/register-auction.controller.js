c6f369509cf21bd1f7094a407feaf9024b9b53e3
Ayush21042002 <51852239+Ayush21042002@users.noreply.github.com>
2020-11-17 20:28:14 +0530

    register auction

--- Backend/controllers/register-auction.controller.js
+++ Backend/controllers/register-auction.controller.js
@@ -3,16 +3,14 @@ const con = require("../database/db");
 exports.registerAuction = (req,res) => {
     const auction = {
         pid : Number(req.body.pid),
-        minBid: Number(req.body.startingPrice),
-        startTime: req.body.start,
-        endTime: req.body.end,
+        minBid: Number(req.body.minBid),
+        startTime: req.body.startTime,
+        endTime: req.body.endTime,
         status: "scheduled"
     };
 
-    con.query("INSERT INTO auctions SET ?",auction, (err,result) => {
+    con.query("INSERT INTO auction SET ?",auction, (err,result) => {
         if(err) throw err;
-
-        console.log(result);
 
         res.status(200).json({
             message: "Successfully scheduled an auction for the product",
